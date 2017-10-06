e0a7f06082cd1e6ab280d229f752c7d785dadb29
Richard Neumann <r.neumann@homeinfo.de>
2017-10-06 16:29:55 +0200

    Updated setup data.

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ from distutils.core import setup
 setup(
     name='boolparse',
     version='latest',
-    author='Richard Neumann',
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info at homeinfo dot de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r dot neumann at homeinfo priod de>',
     py_modules=['boolparse'],
-    license=open('LICENSE').read(),
+    license='GPLv3',
     description='A library for boolean expression parsing')
