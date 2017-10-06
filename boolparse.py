427e85e230b7a90fc0966bf324ee15b38929c797
Richard Neumann <r.neumann@homeinfo.de>
2017-10-06 16:29:47 +0200

    Refactored library.

--- boolparse.py
+++ boolparse.py
@@ -1,123 +1,109 @@
-"""Boolean parser"""
+"""Boolean parser."""
 
-__all__ = ['SecurityError', 'BooleanEvaluator']
+__all__ = ['SecurityError', 'evaluate']
+
+
+OPERATORS = (' and ', ' or ', 'not ', '(', ')')
 
 
 class SecurityError(Exception):
     """Indicates a possible security breach
-    in parsing boolean statements
+    in parsing boolean statements.
     """
 
     pass
 
 
-class BooleanEvaluator():
-    """Class to evaluate boolean expressions"""
+def _callback(string):
+    """Default callback for simple boolean evaluation."""
 
-    def callback(self, x):
-        """Default callback for simple boolean evaluation"""
-        return True if x.upper() == 'TRUE' else False
+    return string.upper() == 'TRUE'
 
-    def __init__(self, string, callback=None, limit=20, operators=None):
-        """Sets the string to be parsed and optionally a
-        callback method and / or an expressions limit.
-        """
-        self.string = string
 
-        if callback is not None:
-            self.callback = callback
+def chklim(statements, limit=None):
+    """Checks whether the limit has been exceeded."""
 
-        self.limit = limit
-        self.operators = operators or (' and ', ' or ', 'not ', '(', ')')
+    if limit is None or statements <= limit:
+        return True
 
-    def __bool__(self):
-        """Returns the string's boolean value"""
-        if eval(self.cmd):
-            return True
+    raise SecurityError('Statement limit exceeded.')
+
+
+def tokenize(string, limit=None, operators=OPERATORS):
+    """Tokenize the string."""
+    buff = max(len(operator) for operator in operators)
+    window = ''
+    statements = 0
+    skip = 0
+
+    for pos, char in enumerate(string):
+        if skip:
+            skip -= 1
         else:
-            return False
+            lookahead = ''
 
-    @property
-    def cmd(self):
-        """Converts the string to a boolean expression in python"""
-        cmdlst = []
-        window = ''
-
-        for token in self.parse():
-            if token:
-                if token in self.operators:
-                    if window:
-                        cmdlst.append(self.evaluate(window))
-                        window = ''
-
-                    cmdlst.append(token)
-                else:
-                    window += token
-
-        if window:
-            cmdlst.append(self.evaluate(window))
-
-        return ' '.join(cmdlst)
-
-    def evaluate(self, statement):
-        """Evaluates the given statement into a boolean value"""
-        callback_result = self.callback(statement)
-
-        if callback_result is True or callback_result is False:
-            return str(callback_result)
-        else:
-            raise SecurityError(
-                'Callback method did not return a boolean value')
-
-    def chklim(self, statements):
-        """Checks whether the limit has not been exceeded"""
-        if self.limit is None:
-            return True
-        elif statements > self.limit:
-            raise SecurityError('Statement limit exceeded')
-        else:
-            return True
-
-    def parse(self):
-        """Explodes the string into a tokens
-        list, considering the limit.
-        """
-        result = []
-        window = ''
-        statements = 0
-        buff = max(len(op) for op in self.operators)
-        skip = 0
-
-        for pos, char in enumerate(self.string):
-            if skip:
-                skip -= 1
-            else:
-                lookahead = ''
-
-                for offset in range(0, buff):
-                    try:
-                        lookahead += self.string[pos + offset]
-                    except IndexError:
-                        window += char
-                        break
-                    else:
-                        if lookahead in self.operators:
-                            if window:
-                                statements += 1
-                                self.chklim(statements)
-                                result.append(window.strip())
-                                window = ''
-
-                            result.append(lookahead)
-                            skip = len(lookahead) - 1
-                            break
-                else:
+            for offset in range(0, buff):
+                try:
+                    lookahead += string[pos + offset]
+                except IndexError:
                     window += char
+                    break
+                else:
+                    if lookahead in operators:
+                        if window:
+                            statements += 1
+                            chklim(statements, limit=limit)
+                            yield window.strip()
+                            window = ''
 
-        # Process possibly remaining window
-        if window:
-            statements += 1
-            self.chklim(statements)
-            result.append(window.strip())
+                        yield lookahead
+                        skip = len(lookahead) - 1
+                        break
+            else:
+                window += char
 
-        return result
+    # Process possibly remaining window
+    if window:
+        statements += 1
+        chklim(statements, limit=limit)
+        yield window.strip()
+
+
+def bool_val(statement, callback=_callback):
+    """Evaluates the given statement into a boolean value."""
+
+    callback_result = callback(statement)
+
+    if callback_result is True or callback_result is False:
+        return str(callback_result)
+
+    raise SecurityError('Callback method did not return a boolean value.')
+
+
+def boolexpr(string, callback=_callback, limit=None, operators=OPERATORS):
+    """Yields boolean expression elements for python."""
+
+    window = ''
+
+    for token in tokenize(string, limit=limit, operators=operators):
+        if token:
+            if token in operators:
+                if window:
+                    yield bool_val(window, callback)
+                    window = ''
+
+                yield token
+            else:
+                window += token
+
+    if window:
+        yield bool_val(window, callback)
+
+
+def evaluate(string, callback=_callback, limit=20, operators=OPERATORS):
+    """Safely evaluates a boolean string."""
+
+    command = ' '.join(boolexpr(
+        string, operators=operators, callback=callback, limit=limit))
+
+    return bool(eval(command))
